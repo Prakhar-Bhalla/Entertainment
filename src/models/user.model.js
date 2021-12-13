aa625d81c2e218e6d07ddea10d5d5891212db10e
Prakhar-Bhalla <prakharbhalla2016@gmail.com>
2021-12-13 17:27:46 +0530

    final commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 const {Schema, model} = require("mongoose");
 
+const bcrypt = require("bcrypt");
+
 const userSchema = new Schema({
     name : {type: String, required: true},
     email : {type: String, required: true, unique: true},
