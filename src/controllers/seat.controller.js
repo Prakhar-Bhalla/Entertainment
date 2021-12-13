a4ca5c65b72dd5ad8ec2bff27e76352d98d9b727
Prakhar-Bhalla <prakharbhalla2016@gmail.com>
2021-12-13 18:42:40 +0530

    final commit

--- src/controllers/seat.controller.js
+++ src/controllers/seat.controller.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const Seat = require("../models/seat.model");
 
+const Show = require("../models/show.model");
+
 router.post("/", async(req, res) => {
     try {
         const seat = await Seat.create(req.body);
@@ -17,6 +19,22 @@ router.get("/show/:id", async(req, res) => {
     try {
         const seats = await Seat.find({show: req.params.id}).populate("show").lean().exec();
         res.status(201).send({seats});
+    } catch(e) {
+        res.status(500).send({status: "failed", message: e.message});
+    }
+})
+
+router.patch("/show/:id", async(req, res) => {
+    try {
+        const seat = await Seat.findOne({show: req.params.id}).populate("show").lean().exec();
+        const show = await Show.findById(seat.show._id).lean().exec();
+        const x = show.total_seats - (+(req.query.seats));
+        if(x<0)
+        {
+            return res.status(201).send("Seats not available");
+        }
+        const updatedShow = await Show.findByIdAndUpdate(seat.show._id, {$set: {total_seats : x}}, {new: true}).lean().exec();
+        res.status(201).send({updatedShow});
     } catch(e) {
         res.status(500).send({status: "failed", message: e.message});
     }
